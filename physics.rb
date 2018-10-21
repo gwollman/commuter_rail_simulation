5b00938cc8525ddb312dff03e1a0a2a438311b26
Garrett Wollman <wollman@csail.mit.edu>
2018-10-20 22:01:48 -0400

    Simulate deceleration (as if it's exactly symmetric to acceleration, which it isn't really), and use this simplified model to simulate the trip between two stations.
    Once we can do this, we can simulate a big chunk of an existing line
    (so long as there are no places where the speed limits are reduced
    between stops).

--- physics.rb
+++ physics.rb
@@ -82,6 +82,76 @@ class TrainPhysics
     end
   end
 
+  # Assumes that the deceleration curve is exactly symmetric
+  # with the acceleration curve, which is not true in reality.
+  # A better model would consider the type of braking system,
+  # thermal limits that might cause one to switch from regen to friction,
+  # leaves on the line, etc.
+  def simulate_stop(step = 1)
+    while (@v > 0)
+      @t += step
+      @s += @v * step
+      a = if (@v <= @v_a_max)
+            @a_max
+          else
+            @k / @v
+          end
+
+      # The train is going to stop, not reverse.
+      if (a * step > @v)
+        a = @v / step
+        @v = 0
+      else
+        @v -= a * step
+      end
+      yield @t, @s, @v, -a
+    end
+  end
+
+  def simulate_dwell(how_long)
+    unless @v == 0
+      raise RuntimeError, "can't dwell at speed"
+    end
+
+    @t += how_long
+  end
+
+  # Yes, I know this is a huge DRY failure, so sue me.
+  def simulate_startstop(v_max, s_max, step = 1)
+    s0 = @s
+    next_stop = s0 + s_max
+
+    yield @t, @s, @v, 0
+    while @v < v_max and (@s + @s - s0) < next_stop
+      @t += step
+      @s += @v * step
+      a = if (@v <= @v_a_max)
+            @a_max
+          else
+            (@k / @v)
+          end
+      if (@v += a * step) > v_max
+        @v = v_max
+      end
+      yield @t, @s, @v, a
+    end
+
+    if (@s + @s - s0) < next_stop
+      puts "reached v_max at s = #{@s}, t = #{@t}, v = #{@v}"
+    end
+    decel_distance = @s - s0
+
+    while (@s + decel_distance) < next_stop
+      @t += step
+      @s += @v * step
+      yield @t, @s, @v, 0
+    end
+
+    puts "starting deceleration at s = #{@s}, t = #{@t}, v = #{@v}"
+
+    simulate_stop {|*args| yield *args }
+  end
+
   def show(fh = STDOUT)
     fh.puts "Mass: #{@m} tonnes"
     fh.puts "Power: #{@p} kW"
@@ -163,36 +233,68 @@ class F40PHconsist < TrainPhysics
   end
 end
 
-DISTANCE = 5000                 # 5 km
-SPEED_LIMIT = 35.32             # 79 mi/h
+SPEED_LIMIT_89 = 39.79          # 89 mi/h ~ 140 km/h
+SPEED_LIMIT_79 = 35.32          # 79 mi/h ~ 130 km/h
+SPEED_LIMIT_69 = 30.85          # 69 mi/h ~ 115 km/h
+SPEED_LIMIT_65 = 29.06          # 65 mi/h
+SPEED_LIMIT_59 = 26.38          # 59 mi/h
 
-$flirt_500_pax = FLIRT_75m_EMU.new(2, 500)
-$flirt_500_pax.show
-puts ""
+SCENARIOS = { "flirt500.tsv" => FLIRT_75m_EMU.new(2, 500),
+              "loco500.tsv" => F40PHconsist.new(3, 500),
+              "loco1600.tsv" => F40PHconsist.new(9, 1600),
+              "loco1600-nodwell.tsv" => F40PHconsist.new(9, 1600) }
 
-File.open("flirt500.tsv", "w") do |fh|
-  $flirt_500_pax.simulate(SPEED_LIMIT, DISTANCE) {|*args| 
-    fh.puts args.join("\t")
-  }
+SCENARIOS.each do |file, model|
+  model.show
+  puts ""
+  dwell_time = 
+    if (model.is_a?(FLIRT_75m_EMU))
+      25
+    else
+      55
+    end
+  dwell_time = 0 if (file =~ /nodwell/)
+
+  File.open(file, "w") do |fh|
+    printit = Proc.new { |*args| fh.puts args.join("\t") }
+
+    # West Station to Boston Landing
+    model.simulate_startstop(SPEED_LIMIT_89, 7500 - 6200, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Boston Landing to Newtonville
+    model.simulate_startstop(SPEED_LIMIT_89, 13300 - 7500, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Newtonville to West Newton
+    model.simulate_startstop(SPEED_LIMIT_89, 15200 - 13300, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # West Newton to Auburndale
+    model.simulate_startstop(SPEED_LIMIT_89, 16900 - 15200, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Auburndale to Wellesley Farms
+    model.simulate_startstop(SPEED_LIMIT_65, 20300 - 16900, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Wellesley Farms to Wellesley Hills
+    model.simulate_startstop(SPEED_LIMIT_69, 21800 - 20300, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Wellesley Hills to Wellesley Square
+    model.simulate_startstop(SPEED_LIMIT_69, 23800 - 21800, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Wellesley Square to Natick
+    model.simulate_startstop(SPEED_LIMIT_79, 28500 - 23800, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # Natick to West Natick
+    model.simulate_startstop(SPEED_LIMIT_89, 32100 - 28500, &printit)
+    model.simulate_dwell(dwell_time)
+
+    # West Natick to Framingham
+    model.simulate_startstop(SPEED_LIMIT_89, 34300 - 32100, &printit)
+  end
 end
-
-$loco_500_pax = F40PHconsist.new(3, 500)
-$loco_500_pax.show
-puts ""
-
-File.open("loco500.tsv", "w") do |fh|
-  $loco_500_pax.simulate(SPEED_LIMIT, DISTANCE) {|*args| 
-    fh.puts args.join("\t")
-  }
-end
-
-$loco_monster = F40PHconsist.new(9, 1600)
-$loco_monster.show
-puts ""
-
-File.open("loco1600.tsv", "w") do |fh|
-  $loco_monster.simulate(SPEED_LIMIT, DISTANCE) {|*args| 
-    fh.puts args.join("\t")
-  }
-end
-
